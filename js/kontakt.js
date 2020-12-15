d6aae4359fea7e7916da06de2cb21957d506a78b
Mixilino <nisavicmiha@gmail.com>
2020-12-15 21:21:57 +0100

    dodavanje kontakt forme i fejl usluge

---
+++ js/kontakt.js
@@ -0,0 +1,54 @@
+function validateForm() {
+    let x = document.forms["myForm"]["ime"].value;
+    if (x.length < 3) {
+        alert("Ime mora imati bar 2 karaktera");
+        return false;
+    }
+    x = document.forms["myForm"]["prezime"].value;
+    if (x.length < 3) {
+        alert("Prezime mora imati bar 2 karaktera");
+        return false;
+    }
+    x = document.getElementById("mejl").value;
+    if (!proveriMejl(x)) {
+        alert("Mejl nije ispravan");
+        return false;
+    }
+    x = document.forms["myForm"]["telefon"].value;
+    if (x.length>15 || x.length<5) {
+        alert("Broj telefona nije validan");
+        return false;
+    }
+    x = document.forms["myForm"]["poruka"].value;
+    console.log(x.length)
+    if (x.length<=1) {
+        alert("Unesite poruku");
+        return false;
+    }
+    if(document.forms["myForm"]["check"].checked === false){
+        alert("Prihvatite uslove koriscenja");
+        return false;
+    }
+    alert("Poslali ste poruku");
+    console.log("AJAX se kao poziva");
+    resetForm();
+    return true;
+}
+
+function proveriMejl(mail){
+    const re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(mail);
+}
+
+function resetForm(){
+    document.forms["myForm"]["ime"].value="";
+    document.forms["myForm"]["prezime"].value="";
+    document.forms["myForm"]["mejl"].value="";
+    document.forms["myForm"]["telefon"].value="";
+    document.forms["myForm"]["poruka"].value="";
+    triggerUslove();
+    return true;
+}
+function triggerUslove(){
+    document.forms["myForm"]["check"].checked = !document.forms["myForm"]["check"].checked;
+}
