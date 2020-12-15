5db43aa7846c6c592b7945b37671364a2656f24b
Mixilino <nisavicmiha@gmail.com>
2020-12-16 00:15:24 +0100

    pokusaj slidera

--- js/usluge.js
+++ js/usluge.js
@@ -1,4 +1,4 @@
 $('.container').slick({
-
+    prevArrow: '<span class="prev"><i class="fa fa-angle-left fa-4x" aria-hidden="true"></i></span>',
+    nextArrow: '<span class="next"><i class="fa fa-angle-right fa-4x" aria-hidden="true"></i></span>'
 });
-console.log("caosaod")
